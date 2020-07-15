f7e5f33ac75a3bcd877a55366bd4cd6d6428e8d6
Ian Oliveira de Medeiros <ianprogrammer@gmail.com.br>
2020-07-14 21:39:37 -0300

    inicial

---
+++ precache-manifest.4cb32f51f0e68f78e6ca788befeeaef3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c207632807392ae1c65d236019c343f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72037838350f3a69ff5c",
+    "url": "/static/css/2.586eee26.chunk.css"
+  },
+  {
+    "revision": "990d01d2f4ecaecec74a",
+    "url": "/static/css/main.9c64c4f9.chunk.css"
+  },
+  {
+    "revision": "72037838350f3a69ff5c",
+    "url": "/static/js/2.62aac1f8.chunk.js"
+  },
+  {
+    "revision": "990d01d2f4ecaecec74a",
+    "url": "/static/js/main.32bc3c45.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
